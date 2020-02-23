433c0cb1c9c5af110d0c42dc11ba7e9f3a9b132d
ahmednmr <ahmed.saleh.nmr@gmail.com>
2020-02-23 19:00:59 +0200

    Esp as aserverto turn on or off lamp from your mobile

--- main.c
+++ main.c
@@ -6,63 +6,155 @@
  */
 
 #include <avr/io.h>
-#include <avr/delay.h>
+#include <util/delay.h>
 #include <stdio.h>
 #include <string.h>
-#include "HAL/Nrf24.h"
 #include "MCAL/UART.h"
+#include <avr/interrupt.h>
+#include "uart.h"
 
-char temp;
-char q = 0;
-char data_array[4];
-char tx_address[5] = {0xD7,0xD7,0xD7,0xD7,0xD7};
-char rx_address[5] = {0xE7,0xE7,0xE7,0xE7,0xE7};
+#define TRUE 1
+#define FALSE 0
+#define DEFAULT_BUFFER_SIZE 1000
+char Rec_Data[DEFAULT_BUFFER_SIZE];
+char parsing_Data[100];
+int Counter=0;
+int REC_Length=0;
 
+
+
+
+char Check_Respond(char * Expected_Respond);
+char Check_Rec_data(void);
+void Clear_REC_Buffer(void);
+
+ISR (USART_RXC_vect)
+{
+	uint8_t oldsrg = SREG;
+	cli();
+	Rec_Data[Counter] = UDR;
+	Counter++;
+	if(Counter == DEFAULT_BUFFER_SIZE){
+		Counter = 0; //pointer = 0;
+	}
+	SREG = oldsrg;
+}
 
 
 int main()
 {
+	char i=0,x=128,y=0,z=0,a=0,b=0,c=0,d=0;
 
-
+	DDRD=(1<<PD4);
+	PORTD |=(1<<PD4);
 	init_UART();
-	UART_SEND_string("System init\r\n");
-	//.......init nrf24.......................................
+	sei();
 
-	init_Nrf24();
 
-	UART_SEND_string("SPI init Done\r\n");
-	//.......write config  nrf24..............................
+	Clear_REC_Buffer();
+	UART_SEND_string("ATE0\r\n");
+	while(!((Check_Respond("\r\nOK\r\n"))||(Check_Respond("ATE0\r\n\r\nOK\r\n"))))
+	{
+		_delay_ms(10);
+	}
 
-	/* Channel #2 , payload length: 4 */
-	nrf24_config(2,4);
-	UART_SEND_string("nrf24 Config done \r\n");
-	//.....................................................
+	UART_SEND_string("AT+CWMODE=3\r\n");
+	while(!Check_Respond("\r\nOK\r\n"))
+	{
+		_delay_ms(10);
+	}
 
-	nrf24_tx_address(tx_address);
-	nrf24_rx_address(rx_address);
 
-	UART_SEND_string("Start Prog\r\n");
+	UART_SEND_string("AT+CIPMUX=1\r\n");
+	while(!Check_Respond("\r\nOK\r\n"))
+	{
+		_delay_ms(10);
+	}
+
+	UART_SEND_string("AT+CIPSERVER=1,80\r\n");
+	while(!((Check_Respond("\r\nOK\r\n"))||(Check_Respond("no change\r\n\r\nOK\r\n"))))
+	{
+		_delay_ms(10);
+	}
+
+
+	UART_SEND_string("AT+CIPSTO=4000\r\n");
+	while(!Check_Respond("\r\nOK\r\n"))
+	{
+		_delay_ms(10);
+	}
+
 
 	while(1)
 	{
 
-
-        if(nrf24_dataReady())
-        {
-            nrf24_getData(data_array);
-            UART_SEND_string("Data recieved ----->>");
-
-            UART_SendChar(data_array[0]);
-
-            UART_SendChar(data_array[1]);
-            UART_SendChar(data_array[2]);
-            UART_SendChar(data_array[3]);
-            UART_SEND_string("\r\n\r\n");
-        }
-
+		Check_Rec_data();
 
 	}
+
+
 	return 0;
 }
+
+
+
+
+char Check_Respond(char * Expected_Respond)
+{
+	char Respond_Length=0;
+
+	Respond_Length=strlen(Expected_Respond);
+
+	if(strncmp(Rec_Data,Expected_Respond, Respond_Length)==0)
+	{
+		Clear_REC_Buffer();
+		return TRUE;
+	}
+	return FALSE;
+
+
+}
+
+
+
+char Check_Rec_data(void)
+{
+
+	REC_Length= strlen(Rec_Data);
+
+
+
+	if(REC_Length>90)
+	{
+		memcpy (parsing_Data, Rec_Data, 90*sizeof(char));
+
+
+		if(strstr(Rec_Data,"turnon") != 0)
+		{
+			PORTD &=~(1<<PD4);
+		}
+
+		else if(strstr(Rec_Data,"turnoff") != 0)
+		{
+			PORTD |=(1<<PD4);
+		}
+
+		memset(Rec_Data,0,REC_Length);
+		memset(parsing_Data,0,100);
+		Counter=0;
+		REC_Length=0;
+
+		return TRUE;
+	}
+	return FALSE;
+}
+
+
+void Clear_REC_Buffer(void)
+{
+	Counter=0;
+	memset(Rec_Data,0,DEFAULT_BUFFER_SIZE);
+}
+
 
 
